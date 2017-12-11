7b81607a5da8e19102b61f4b023e88ccd80bc91c
Nicole Thorly <nicolethorly@gmail.com>
2017-12-10 21:44:20 -0600

    add comments and adjust names

--- js/main.js
+++ js/main.js
@@ -5,40 +5,54 @@ const MAIN_CONTAINER_ID = 'main-container';
 const SELECTED_CLASS = 'selected';
 const OPEN_CLASS = 'open';
 
+// when dom content has loaded, run callback
 document.addEventListener("DOMContentLoaded", function() {
+    //looping through the nav buttons
     document.querySelectorAll('.leftnav .nav-button').forEach((navBtn) => {
+        //if the current nav button is 'hurricane-harvey' load that template
         if (navBtn.id === 'hurricane-harvey') {
             setTemplate(navBtn.id);
         }
-
+        //for each nav button add click handler to dynamically change main content
         navBtn.addEventListener('click', (event) => {
             let navButton = event.target;
+            //if we click the currently selected nav button, do nothing
             if (navButton.classList.contains(SELECTED_CLASS)) return;
             setSelected(navButton);
             setTemplate(navButton.id);
         });
     });
+    //looping through the nav headers
     document.querySelectorAll('.leftnav .nav-button-container .title').forEach((navBtnContainer) => {
+        //for each nav header add click handler to expand/collapse the side nav sections
         navBtnContainer.addEventListener('click', (event) => {
             event.preventDefault();
             let navButtonContainer = event.target;
+            //if we click the currently opened nav section, do nothing
             if (navButtonContainer.classList.contains(OPEN_CLASS)) return;
             setOpen(navButtonContainer);
         });
     });
 });
 
+//function to toggle open nav section
 function setOpen(selectedContainer) {
     selectedContainer.parentElement.parentElement.getElementsByClassName(OPEN_CLASS)[0].classList.remove(OPEN_CLASS);
     selectedContainer.parentElement.classList.add(OPEN_CLASS);
 }
 
+//function to toggle selected nav button
 function setSelected(selectedButton) {
     selectedButton.parentElement.parentElement.getElementsByClassName(SELECTED_CLASS)[0].classList.remove(SELECTED_CLASS);
     selectedButton.classList.add(SELECTED_CLASS);
 }
 
-function getTemplatePathById(id) {
+/**
+ * returning template/script information for selected nav
+ * @param id - id of nav button clicked
+ * @returns {{script: *, template: *}} - object containing js and html template information for dynamic content
+ */
+function getTemplateObjectById(id) {
     // instantiate template object to return
     let templateObj = {
         script: void(0),
@@ -82,8 +96,12 @@ function getTemplatePathById(id) {
     return templateObj;
 }
 
+/**
+ * a function to retrieve the html templates and inject into main container
+ * @param templateId
+ */
 function setTemplate(templateId) {
-    const templateObj = getTemplatePathById(templateId)
+    const templateObj = getTemplateObjectById(templateId)
     let httpRequest = new XMLHttpRequest();
     httpRequest.open('GET', 'views/' + templateObj.template);
     httpRequest.send(null);
@@ -94,7 +112,7 @@ function setTemplate(templateId) {
         if (httpRequest.readyState === DONE) {
             if (httpRequest.status === OK) {
                 document.getElementById(MAIN_CONTAINER_ID).innerHTML = httpRequest.responseText;
-                injectScript(templateObj.script);
+                injectHarveyMapScript(templateObj.script);
             } else {
                 console.error('Template for path, ' + path + ', not found');
             }
@@ -102,7 +120,11 @@ function setTemplate(templateId) {
     };
 }
 
-function injectScript(script) {
+/**
+ * a function to replace the current map script with newly selected nav button map script
+ * @param script
+ */
+function injectHarveyMapScript(script) {
     removeHarveyMapScript();
     if (script) {
         let scriptElement = document.createElement('script'); // <script></script>
@@ -112,6 +134,9 @@ function injectScript(script) {
     }
 }
 
+/**
+ * a function to remove the current map script before injecting the new map script
+ */
 function removeHarveyMapScript() {
     window.dispatchEvent(new Event('destroy-arc-gis-widgets'));
     let harveyScript = document.getElementById(INJECTED_SCRIPT_ID);
